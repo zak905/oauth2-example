b8f54488e29b242bfb279a93b1b471a777c96884
zak905 <zakaria.amine88@gmail.com>
2018-03-01 21:31:50 +0100

    add global method security

--- resource-server/src/main/java/com/gwidgets/examples/resourceserver/ResourceController.java
+++ resource-server/src/main/java/com/gwidgets/examples/resourceserver/ResourceController.java
@@ -1,28 +1,35 @@
 package com.gwidgets.examples.resourceserver;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController("/")
 public class ResourceController {
 
+    @PreAuthorize("#oauth2.hasScope('read')")
     @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
 
+    @PreAuthorize("#oauth2.hasScope('read')")
     @GetMapping("/foo")
     public String foo(){
         return "foo";
     }
 
-    @GetMapping("/bar")
+    @PreAuthorize("#oauth2.hasScope('write')")
+    @PostMapping("/bar")
     public String bar(){
         return "bar";
     }
 
-    @GetMapping("/test")
+    @PreAuthorize("#oauth2.hasScope('trust')")
+    @DeleteMapping("/test")
     public String test(){
         return "test";
     }
--- resource-server/src/main/java/com/gwidgets/examples/resourceserver/ResourceSecurityConfiguration.java
+++ resource-server/src/main/java/com/gwidgets/examples/resourceserver/ResourceSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.gwidgets.examples.resourceserver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -14,6 +15,7 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
  */
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceSecurityConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
